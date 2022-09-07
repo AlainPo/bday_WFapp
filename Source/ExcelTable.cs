9696827b076f1bc629940c4be490142537875417
AlainPo <109284542+AlainPo@users.noreply.github.com>
2022-09-07 10:46:56 +0700

    Add files via upload

---
+++ Source/ExcelTable.cs
@@ -0,0 +1,206 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using ClosedXML.Excel;
+using System.Runtime.Remoting.Messaging;
+
+namespace bdays_app
+{
+    public class ExcelTable
+    {
+        public string path = "";    //путь к файлу
+        FileStream stream;
+        IExcelDataReader reader;
+        DataSet db;
+        DataTable table;     
+
+
+        public ExcelTable(string path = "")
+        {
+            this.path = path;
+        }
+
+        //открыть файл xlsx 
+        public DataTableCollection OpenFile(DataTableCollection tableCollection)
+        {
+            stream = File.Open(path, FileMode.Open);
+
+            reader = ExcelReaderFactory.CreateReader(stream);
+            db = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            }
+            );
+
+            tableCollection = db.Tables;
+
+            return tableCollection;
+        }
+
+        //открыть лист в dataGridView
+        public void OpenSheet(DataTableCollection tableCollection, DataGridView dataGridView1, int Sheet)
+        {
+            dataGridView1.DataSource = tableCollection[Sheet];
+        }
+
+        //считывание таблицы построчно, сортировка дат по степени близости
+        public void FindComing(DataTableCollection tableCollection, int Sheet)
+        {
+            table = tableCollection[Sheet];
+            foreach (DataRow row in table.Rows)
+            {
+                string[] record = {row[0].ToString(), Convert.ToDateTime(row[1]).ToShortDateString()};
+
+                AddToTables(tableCollection, record); //проверяем основную таблицу на наличие необходимых дат 
+            }
+        }
+
+        //изменение даты рождения или удаление записи 
+        public bool ChangeRecord(DataTableCollection tableCollection, int Sheet, string[] record, bool action) //1 - change, 2 - delete
+        {
+            bool flag = false;
+            table = tableCollection[Sheet];
+            foreach (DataRow row in table.Rows)
+            {
+                if (record[0].ToString().ToLower().Trim() == row[0].ToString().ToLower().Trim())
+                {
+                    if (action)
+                    {
+                        row[1] = record[1];
+                        Recheck(tableCollection, Sheet, record, row); //добавляем или удаляем из остальных таблиц
+                        flag = true;
+                        return flag;
+                    }
+                    else
+                    {   if (Convert.ToDateTime(record[1]).ToShortDateString() == Convert.ToDateTime(row[1]).ToShortDateString())
+                        table.Rows.Remove(row);
+                        flag = true;
+                        return flag;
+                    }
+                }
+            }
+            return flag;
+        }
+
+        //добавление записи
+        public void AddRecord(DataTableCollection tableCollection, int Sheet, string[] record)
+        {
+            tableCollection[Sheet].Rows.Add(record);           
+        }
+
+        //добавление записи в табл ближайших или сегодняшних др
+        public void AddToTables(DataTableCollection tableCollection, string[] record)
+        {
+            if (Convert.ToDateTime(record[1]).Day == DateTime.Today.Day
+                    && Convert.ToDateTime(record[1]).Month == DateTime.Today.Month)
+            {
+                AddRecord(tableCollection, 1, record);
+            }
+            else if (CheckSoonBday(record))
+            {
+                AddRecord(tableCollection, 2, record);
+            }
+        }
+
+        //провека измененной записи на выполнение условий
+        public void Recheck(DataTableCollection tableCollection, int Sheet, string[] record, DataRow row)
+        {
+            table = tableCollection[Sheet];
+
+            if (Sheet == 1 && (Convert.ToDateTime(record[1]).Day != DateTime.Today.Day || Convert.ToDateTime(record[1]).Month != DateTime.Today.Month))
+            {
+                table.Rows.Remove(row);
+                if (!IsInTable(tableCollection, Sheet, record)) 
+                    AddToTables(tableCollection, record);
+            }
+            else
+                if (Sheet == 2 && !CheckSoonBday(record))
+            {
+                table.Rows.Remove(row);
+                if (!IsInTable(tableCollection,Sheet,record))
+                    AddToTables(tableCollection, record);
+            }
+        }
+
+        //проверка, скоро ли др
+        public bool CheckSoonBday(string[] record)
+        {
+            DateTime checkDate = new DateTime(DateTime.Today.Year, Convert.ToInt32(Convert.ToDateTime(record[1]).Month), Convert.ToInt32(Convert.ToDateTime(record[1]).Day));
+
+            if (checkDate.Date > DateTime.Today && checkDate.Date < DateTime.Today.AddDays(7))
+                return true;
+            else return false;
+        }
+
+        //проверк, есть ли запись в таблице
+        public bool IsInTable (DataTableCollection tableCollection, int Sheet, string[] record)
+        {
+            table = tableCollection[Sheet];
+
+            foreach (DataRow row in table.Rows)
+            {
+                if (record[0].ToString().ToLower().Trim() == row[0].ToString().ToLower().Trim())
+                    return true;
+            }
+            return false;
+        }
+
+        //сохранение таблицы
+        public void SaveSheet(DataGridView dataGridView, DataTableCollection tableCollection, int Sheet)
+        {
+            stream.Close();
+
+            int rowNum = dataGridView.RowCount;
+
+            var workbook = new XLWorkbook(path);
+            var ws = workbook.Worksheet(++Sheet);
+
+            int column = 0;
+            int row=0;
+
+            ws.Clear();
+
+            ws.Range("A1").Value = "Name";
+            ws.Range("B1").Value = "BDay";
+
+            if (rowNum == 0) {
+                ws.Columns().AdjustToContents();
+                workbook.SaveAs(path);
+                return; 
+            }
+
+            var rngNumbers = ws.Range($"A2:A"+ rowNum);
+            foreach (var cell in rngNumbers.Cells() )
+            {
+                string formattedString = dataGridView[column, row].Value.ToString();
+                cell.DataType = XLDataType.Text;
+                cell.Value = formattedString;
+                row++;
+            }
+            column++;
+            row = 0;
+            rngNumbers = ws.Range($"B2:B" + rowNum);
+            foreach (var cell in rngNumbers.Cells())
+            {
+                DateTime formattedString = Convert.ToDateTime(dataGridView[column, row].Value).Date;
+                cell.DataType = XLDataType.DateTime;
+                cell.Value = formattedString;
+                row++;
+            }
+
+            ws.Columns().AdjustToContents();
+
+            workbook.SaveAs(path);
+        }
+    }
+}
---
+++ Source/Form1.cs
@@ -0,0 +1,103 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bdays_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ExcelTable Tab =  new ExcelTable("..\\bdays.xlsx");
+
+        public DataTableCollection tableCollection = null;
+
+        //при загрузке формы вывод 3х таблиц: 1 полной и 2 с выбранными данными
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            tableCollection = Tab.OpenFile(tableCollection);
+            tableCollection[1].Clear();
+            tableCollection[2].Clear();
+
+            Tab.FindComing(tableCollection, 0); //ищем ближ др
+            Tab.OpenSheet(tableCollection, dataGridView1, 1);   //др сегодня
+            Tab.OpenSheet(tableCollection, dataGridView2, 2);   //др ближ 7 дней
+            Tab.OpenSheet(tableCollection, dataGridView3, 0);   //все др
+        }
+
+        //добавление записи
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] record = {textBox1.Text, dateTimePicker1.Value.Date.ToString()};
+            if (Tab.IsInTable(tableCollection, 0, record))
+            {
+                MessageBox.Show("Такая запись уже есть..", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return;
+            }
+            Tab.AddRecord(tableCollection, 0, record); //добавить в осн табл
+            Tab.AddToTables(tableCollection, record); //меняем доп таблицы
+        }
+
+        //редактирование даты рождения по имени
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] record = { textBox1.Text, dateTimePicker1.Value.Date.ToString() }; //введенные в форме данные
+            bool flag = Tab.ChangeRecord(tableCollection, 0, record, true); //проверка на их существование в таблице
+            if (!flag)
+            {
+                MessageBox.Show("Такой записи нет..", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return;
+            }
+            //изменение доп таблиц
+            if (Tab.IsInTable(tableCollection, 1, record))
+                Tab.ChangeRecord(tableCollection, 1, record, true);
+            else if (Tab.IsInTable(tableCollection, 2, record))
+                Tab.ChangeRecord(tableCollection, 2, record, true);
+            else
+                Tab.AddToTables(tableCollection, record);
+        }
+
+        //удаление записи по имени и дате рождения
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] record = { textBox1.Text, dateTimePicker1.Value.Date.ToString() }; //требуемая запись
+            bool flag = Tab.ChangeRecord(tableCollection, 0, record, false);//проверка на существование удаление в осн таблице
+            if (!flag)
+            {
+                MessageBox.Show("Такой записи нет..", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return;
+            }
+            //удаление в доп таблицах при наличии
+            Tab.ChangeRecord(tableCollection, 1, record, false);
+            Tab.ChangeRecord(tableCollection, 2, record, false);
+        }
+
+        //сохранение таблицы по листам
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Tab.SaveSheet(dataGridView3, tableCollection, 0);
+                Tab.SaveSheet(dataGridView1, tableCollection, 1);
+                Tab.SaveSheet(dataGridView2, tableCollection, 2);
+                MessageBox.Show("Успешно сохранено!!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
